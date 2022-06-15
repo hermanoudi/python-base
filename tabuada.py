b6652bfaf6a03b432e3e0564f558f0ca18227f59
Hermano Flávio <hermanoudi@gmail.com>
2022-06-15 15:29:38 -0300

    tabuada

---
+++ tabuada.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Imprime a tabuada do 1 ao 10.
+
+Tabuada do 1
+1
+2
+3
+...
+
+Tabuada do 2
+1
+2
+3
+...
+
+"""
+__version__ = "0.1.0"
+__author__ = "Hermano"
+
+#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
+
+# Iterable (percorriveis)
+for numero in numeros:
+    print("Tabuada do:", numero)
+    for outro_numero in numeros:
+        print(numero * outro_numero)
+    print("---------------")
