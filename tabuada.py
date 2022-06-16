adb0d3325b7a8a181fe1ed424fd3ddc09ece3f16
Hermano Flávio <hermanoudi@gmail.com>
2022-06-16 18:27:45 -0300

    Formatação de texto na tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 """Imprime a tabuada do 1 ao 10.
 
-Tabuada do 1
-1
-2
-3
-...
+---Tabuada do 1---
 
-Tabuada do 2
-1
-2
-3
+     1 x 1 = 1
+     2 x 2 = 2
+     3 x 1 = 3
+...
+##################
+---Tabuada do 2---
+
+     {operacoes}
 ...
 
 """
@@ -18,11 +18,15 @@ __version__ = "0.1.0"
 __author__ = "Hermano"
 
 #numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Iterable (percorriveis)
 numeros = list(range(1, 11))
 
-# Iterable (percorriveis)
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("---------------")
+# para cada numero em numeros:
+for n1 in numeros:
+    print("{:-^18}".format(f" Tabuada do {n1} "))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
+   # print(template_base.format(operacao=operacao))
