42b2a396461f055cd4d2016cdf1c8340457b3663
Hermano Flávio <hermanoudi@gmail.com>
2022-06-20 17:14:50 -0300

    Script escola_v2_com_sets.py para treinar sets e conjuntos que não se repetem e não ordenam

---
+++ escola_v2_com_sets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Exibe relatório de crianças por atividade.
+
+Imprimir a lista de crianças agrupadas por sala 
+que frequenta cada uma das atividades.
+"""
+__version__ = "0.1.1"
+
+sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
+sala2 = ["João", "Antonio", "Carlos", "Maria", "Isolda"]
+
+aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
+aula_musica = ["Erik", "Carlos", "Maria"]
+aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+
+atividades = [("Ingles", aula_ingles),
+    ("Música", aula_musica),
+    ("Dança", aula_danca),
+]
+
+# Listar alunos em cada atividade por sala
+for nome_atividade, atividade in atividades:
+    print(f"Alunos da atividade {nome_atividade}\n")
+    print("#" * 40)
+    # sala1 que tem interseção com a atividade
+    atividade_sala1 = set(sala1) & set(atividade)
+    atividade_sala2 = set(sala2).intersection(set(atividade))
+
+    
+    print(f"{nome_atividade} sala1 ", atividade_sala1)
+    print(f"{nome_atividade} sala2 ", atividade_sala2)
+    print()
+    print("#" * 40)
+ 
